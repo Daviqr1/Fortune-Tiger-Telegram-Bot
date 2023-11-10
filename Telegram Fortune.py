2690015fe2bff93620e5e1855a37f94dc94545e2
Davi Barros de Rezende <84293017+Daviqr1@users.noreply.github.com>
2023-11-10 13:12:28 -0300

    Update Telegram Fortune.py

--- Telegram Fortune.py
+++ Telegram Fortune.py
@@ -7,8 +7,8 @@ import time
 import bd
 from pytz import timezone
 
-api_key = '6703477631:AAH50qOf0K2Ofyq9BGH6I8n8gcjZ5A6-73s'
-chat_id = '-1002055610444'
+api_key = 'insert-api-key'
+chat_id = 'insert-chat-id'
 
 
 LINK_SITE = ''
